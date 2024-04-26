af7920ad4ba1871dd165840a2f3ab46b0b0fa441
shmoon57 <tjdgk253@gmail.com>
2024-04-26 20:39:45 +0900

    prac 18 time clock 실습

---
+++ 240419_MyFristProgram/Prac18.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <time.h>
+
+using namespace std;
+
+/*int main()
+{
+	time_t start, end;
+	double result;
+
+	start = time(NULL); // 현재 시간 반환
+
+	int a = -9999999999;
+	while (true)
+	{
+		a++;
+
+		if (a == 999999999)
+		{
+			break;
+		}
+	}
+
+
+	end = time(NULL);
+
+	result = (double)(end - start);
+
+	cout << "수행시간 : " << result << "second" << "\n";
+
+	return 0;
+	
+} */
+
+int main()
+{
+	clock_t start, end;
+	double result;
+
+	start = clock();
+
+	int a = -9999999999;
+	while (true)
+	{
+		a++;
+
+		if (a == 999999999)
+		{
+			break;
+		}
+	}
+
+	end = clock();
+	result = double(end - start);
+
+	cout << "수행시간 : " << result << "milisecond" << "\n";
+
+	return 0;
+}
