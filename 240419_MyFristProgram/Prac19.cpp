b1282b3f0fbf614ca7c7ff50b413506ccab1b0ce
shmoon57 <tjdgk253@gmail.com>
2024-04-29 15:10:24 +0900

    실습 7 Vectort 성능체크

--- 240419_MyFristProgram/Prac19.cpp
+++ 240419_MyFristProgram/Prac19.cpp
@@ -1,5 +1,6 @@
 // 실습 7 vector 성능 체크
 
+
 #include <iostream>
 #include <vector> 
 #include <time.h>
@@ -14,6 +15,7 @@ void main()
 
 	vector<int> test_vec1(1); // 크기 1 정수형 벡터 생성
 
+	// pushback 1억회 수행 , 소요시간을 ms 단위로 출력
 	start = clock();
 
 	for (int i = 0; i < 100000000; i++)
@@ -27,10 +29,13 @@ void main()
 
 	cout << "test_vec1 수행시간 : " << result << "(ms)" << endl;
 
-	vector<int> test_vec2(1); // 크기 1 정수형 벡터 생성
+	// 크기 1 정수형 벡터 생성
+	vector<int> test_vec2(1); 
 
-	test_vec2.reserve(100000000); // reserve() 1억개 공간 예약
+	// reserve() 1억개 공간 예약
+	test_vec2.reserve(100000000); 
 
+	// pushback 1억회 수행 , 소요시간을 ms 단위로 출력
 	start = clock();
 
 	for (int i = 0; i < 100000000; i++)
