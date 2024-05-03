d0b92b08f941f75fe37353062c0a0423635c4282
shmoon57 <tjdgk253@gmail.com>
2024-05-03 19:48:46 +0900

    살숩 prac 28 namespace 사용해보기

--- 240419_MyFristProgram/MyFunctions.cpp
+++ 240419_MyFristProgram/MyFunctions.cpp
@@ -3,34 +3,38 @@
 #include<string>
 #include <vector>
 
+namespace sh_func
+{
+
 #define AGE 1
 
-using namespace std;
+	using namespace std;
 
-void printStudentInfo(string** info, int count)// 학생 정보 출력 함수 , 2차원 동적배열 값 확인
-{
-	cout << "<학생 정보를 출력합니다>" << endl << endl;
-	cout << "이름 나이 생일" << endl;
-
-	for (int i = 0; i < count; i++)
+	void printStudentInfo(string** info, int count)// 학생 정보 출력 함수 , 2차원 동적배열 값 확인
 	{
-		for (int j = 0; j < 3; j++)
+		cout << "<학생 정보를 출력합니다>" << endl << endl;
+		cout << "이름 나이 생일" << endl;
+
+		for (int i = 0; i < count; i++)
 		{
-			cout << info[i][j] << ' ';
+			for (int j = 0; j < 3; j++)
+			{
+				cout << info[i][j] << ' ';
+			}
+			cout << endl;
 		}
-		cout << endl;
 	}
-}
 
-void PrintSumAge(string** info, int count, int sum)
-{
-	for (int i = 0; i < count; i++)
+	void PrintSumAge(string** info, int count, int sum)
 	{
-		sum = sum + stoi(info[i][AGE]); // 나이 합 구하기
+		for (int i = 0; i < count; i++)
+		{
+			sum = sum + stoi(info[i][AGE]); // 나이 합 구하기
+		}
+
+		cout << endl << "나이의 합 :" << sum << endl;
+
+		cout << "평균 나이 : " << float(sum / count) << endl; // 실수형으로 변환
+
 	}
-
-	cout << endl << "나이의 합 :" << sum << endl;
-
-	cout << "평균 나이 : " << float(sum / count) << endl; // 실수형으로 변환
-
 }
--- 240419_MyFristProgram/MyFunctions.h
+++ 240419_MyFristProgram/MyFunctions.h
@@ -4,15 +4,20 @@
 
 using namespace std;
 
+namespace sh_func
+{
+
 #ifndef _Print_
 #define _Print_
 
-/*중복 선언되면 안되는 코드들*/
+	/*중복 선언되면 안되는 코드들*/
 
-// 1. 학생 정보 출력 함수  선언
-void printStudentInfo(string** info, int count);
+	// 1. 학생 정보 출력 함수  선언
+	void printStudentInfo(string** info, int count);
 
-// 2. 평균 나이 구하는 함수 선언
-void PrintSumAge(string** info, int count, int sum);
+	// 2. 평균 나이 구하는 함수 선언
+	void PrintSumAge(string** info, int count, int sum);
 
 #endif 
+
+}
