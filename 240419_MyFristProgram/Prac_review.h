11a29f0be3a528d863340bb2e864374d80188423
shmoon57 <tjdgk253@gmail.com>
2024-05-17 17:25:24 +0900

    종합 실습 + 파일 분리 + namespace 1개 cpp로 수정

--- 240419_MyFristProgram/Prac_review.h
+++ 240419_MyFristProgram/Prac_review.h
@@ -6,13 +6,10 @@ using namespace std;
 
 namespace sh_func
 {
-
 #ifndef _Print_
 #define _Print_
 
-	/*중복 선언되면 안되는 코드들*/
-
-	// 1. 학생 정보 출력 함수  선언
+	// 1. 학생 정보 출력 함수 선언
 	void printStudentInfo(string** info, int count);
 
 	// 2. 평균 나이 구하는 함수 선언
